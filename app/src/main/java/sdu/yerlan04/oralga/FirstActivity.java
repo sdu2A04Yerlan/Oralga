3a6e44571ff1572032a383979dc07dc2700f9c10
Yerlan04 <yerlan04@gmail.com>
2019-08-07 15:41:48 +0500

    Initial commit

---
+++ app/src/main/java/sdu/yerlan04/oralga/FirstActivity.java
@@ -0,0 +1,6 @@
+package sdu.yerlan04.oralga;
+
+import android.app.Activity;
+
+public class FirstActivity extends Activity {
+}
---
+++ app/src/main/java/sdu/yerlan04/oralga/MainActivity.java
@@ -0,0 +1,12 @@
+package sdu.yerlan04.oralga;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
